27ff3caba530f4a637f4bb67b624aeac27d4ad88
Michele Lora <michele_lora@sutd.edu.sg>
2020-01-22 19:27:34 +0800

    [LTLContracts] Connected to the project.

---
+++ src/LTLContracts/src/main.cc
@@ -0,0 +1,13 @@
+/**
+ * @author      <a href="mailto:michele.lora@univr.it">Michele Lora</a>
+ * @date        2019
+ *              This project is released under the 3-Clause BSD License.
+ * */
+
+#include "Chase.hh"
+
+
+int main( int argc, char * argv[] )
+{
+    chase::messageError("Compiled.");
+}
