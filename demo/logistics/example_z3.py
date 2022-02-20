4f67366d5861d973a78767b086ec85b7698e596c
Michele Lora <michele.lora@univr.it>
2022-02-19 19:02:07 -0800

    ongoing debug of logistics case study

--- demo/logistics/example_z3.py
+++ demo/logistics/example_z3.py
@@ -17,16 +17,25 @@ node_4_F0_out = Real('node_4_F3_out')
 ## assumptions:
 node_4_assumptions = And(
     node_4_F1_in + node_4_F2_in + node_4_F3_in + node_4_F0_in <= 1,
-    node_4_F1_in + node_4_F2_in + node_4_F3_in + node_4_F0_in >= 0)
+    node_4_F1_in + node_4_F2_in + node_4_F3_in + node_4_F0_in >= 0,
+    node_4_F0_in >= 0, node_4_F0_in <= 1,
+    node_4_F1_in >= 0, node_4_F1_in <= 1,
+    node_4_F2_in >= 0, node_4_F2_in <= 1,
+    node_4_F3_in >= 0, node_4_F3_in <= 1
+    )
 
 ## guarantee:
 node_4_guarantees = And(
     node_4_F1_out + node_4_F2_out + node_4_F3_out + node_4_F0_out <= 1,
     node_4_F1_out + node_4_F2_out + node_4_F3_out + node_4_F0_out >= 0,
-    node_4_F0_out <= 0,
-    node_4_F1_out >= 0,
-    node_4_F2_out >= 0,
-    node_4_F3_out >= 0,
+    node_4_F0_out >= 0, node_4_F0_out <= 1,
+    node_4_F1_out >= 0, node_4_F1_out <= 1,
+    node_4_F2_out >= 0, node_4_F2_out <= 1,
+    node_4_F3_out >= 0, node_4_F3_out <= 1,
+    node_4_F0_out <= node_4_F0_in,
+    node_4_F1_out >= node_4_F1_in,
+    node_4_F2_out == node_4_F2_in,
+    node_4_F3_out == node_4_F3_in,
     node_4_F1_in + node_4_F2_in + node_4_F3_in + node_4_F0_in == node_4_F1_out +
     node_4_F2_out + node_4_F3_out + node_4_F0_out)
 
@@ -48,17 +57,27 @@ node_5_F0_out = Real('node_5_F3_out')
 ## assumptions:
 node_5_assumptions = And(
     node_5_F1_in + node_5_F2_in + node_5_F3_in + node_5_F0_in <= 1,
-    node_5_F1_in + node_5_F2_in + node_5_F3_in + node_5_F0_in >= 0)
+    node_5_F1_in + node_5_F2_in + node_5_F3_in + node_5_F0_in >= 0,
+    node_5_F0_in >= 0, node_5_F0_in <= 1,
+    node_5_F1_in >= 0, node_5_F1_in <= 1,
+    node_5_F2_in == 0, 
+    node_5_F2_in <= 1,
+    node_5_F3_in >= 0, node_5_F3_in <= 1)
 
 ## guarantee:
 node_5_guarantees = And(
     node_5_F1_out + node_5_F2_out + node_5_F3_out + node_5_F0_out <= 1,
     node_5_F1_out + node_5_F2_out + node_5_F3_out + node_5_F0_out >= 0,
-    node_5_F0_out <= 0,
-    node_5_F1_out >= 0,
-    node_5_F2_out >= 0,
-    node_5_F3_out >= 0,
-    node_5_F1_in + node_5_F2_in + node_5_F3_in + node_5_F0_in == node_5_F1_out + node_5_F2_out + node_5_F3_out + node_5_F0_out)
+    node_5_F0_out >= 0, node_5_F0_out <= 1,
+    node_5_F1_out >= 0, node_5_F1_out <= 1,
+    node_5_F2_out >= 0, node_5_F2_out <= 1,
+    node_5_F3_out >= 0, node_5_F3_out <= 1,
+    node_5_F0_out <= node_5_F0_in,
+    node_5_F2_out >= node_5_F2_in,
+    node_5_F1_out == node_5_F1_in,
+    node_5_F3_out == node_5_F3_in,
+    node_5_F1_in + node_5_F2_in + node_5_F3_in + node_5_F0_in == node_5_F1_out +
+    node_5_F2_out + node_5_F3_out + node_5_F0_out)
 
 ################################################################################
 ################################################################################
@@ -73,21 +92,32 @@ node_6_F0_in = Real('node_6_F0_in')
 node_6_F1_out = Real('node_6_F1_out')
 node_6_F2_out = Real('node_6_F2_out')
 node_6_F3_out = Real('node_6_F3_out')
-node_6_F0_out = Real('node_6_F3_out')
+node_6_F0_out = Real('node_6_F0_out')
 
 ## assumptions:
 node_6_assumptions = And(
     node_6_F1_in + node_6_F2_in + node_6_F3_in + node_6_F0_in <= 1,
-    node_6_F1_in + node_6_F2_in + node_6_F3_in + node_6_F0_in >= 0)
+    node_6_F1_in + node_6_F2_in + node_6_F3_in + node_6_F0_in >= 0,
+    node_6_F3_in >= 0, node_6_F3_in <= 1,
+    #node_6_F3_in == 0,
+    node_6_F0_in >= 0, node_6_F0_in <= 1,
+    node_6_F1_in >= 0, node_6_F1_in <= 1,
+    node_6_F2_in >= 0, node_6_F2_in <= 1
+    )
+    
 
 ## guarantee:
 node_6_guarantees = And(
     node_6_F1_out + node_6_F2_out + node_6_F3_out + node_6_F0_out <= 1,
     node_6_F1_out + node_6_F2_out + node_6_F3_out + node_6_F0_out >= 0,
-    node_6_F0_out <= 0,
-    node_6_F1_out >= 0,
-    node_6_F2_out >= 0,
-    node_6_F3_out >= 0,
+    node_6_F0_out >= 0, node_6_F0_out <= 1,
+    node_6_F1_out >= 0, node_6_F1_out <= 1,
+    node_6_F2_out >= 0, node_6_F2_out <= 1,
+    node_6_F3_out >= 0, node_6_F3_out <= 1,
+    node_6_F0_out <= node_6_F0_in,
+    node_6_F3_out >= node_6_F3_in,
+    node_6_F1_out == node_6_F1_in,
+    node_6_F2_out == node_6_F2_in,
     node_6_F1_in + node_6_F2_in + node_6_F3_in + node_6_F0_in == node_6_F1_out + node_6_F2_out + node_6_F3_out + node_6_F0_out)
 
 ################################################################################
@@ -108,14 +138,25 @@ node_0_F0_out = Real('node_0_F3_out')
 ## assumptions:
 node_0_assumptions = And(
     node_0_F1_in + node_0_F2_in + node_0_F3_in + node_0_F0_in <= 1,
-    node_0_F1_in + node_0_F2_in + node_0_F3_in + node_0_F0_in >= 0)
+    node_0_F1_in + node_0_F2_in + node_0_F3_in + node_0_F0_in >= 0,
+    node_0_F1_in >= 0, node_0_F1_in <= 1,
+    node_0_F2_in >= 0, node_0_F2_in <= 1,
+    node_0_F3_in >= 0, node_0_F3_in <= 1,
+    node_0_F0_in >= 0, node_0_F0_in <= 1)
 
 ## guarantees:
 node_0_guarantees = And(
     node_0_F0_in == node_0_F0_out,
-        node_0_F1_in == node_0_F1_out,
-            node_0_F2_in == node_0_F2_out,
-                node_0_F3_in == node_0_F3_out)
+    node_0_F1_in == node_0_F1_out,
+    node_0_F2_in == node_0_F2_out,
+    node_0_F3_in == node_0_F3_out,
+    node_0_F1_out >= 0, node_0_F1_out <= 1,
+    node_0_F2_out >= 0, node_0_F2_out <= 1,
+    node_0_F3_out >= 0, node_0_F3_out <= 1,
+    node_0_F0_out >= 0, node_0_F0_out <= 1,
+    node_0_F1_out + node_0_F2_out + node_0_F3_out + node_0_F0_out <= 1,
+    node_0_F1_out + node_0_F2_out + node_0_F3_out + node_0_F0_out >= 0,
+    node_0_F1_in + node_0_F2_in + node_0_F3_in + node_0_F0_in == node_0_F1_out + node_0_F2_out + node_0_F3_out + node_0_F0_out)
 
 ################################################################################
 ################################################################################
@@ -128,21 +169,32 @@ node_7_F0_in = Real('node_7_F0_in')
 
 ## output variables:
 node_7_F1_out = node_6_F1_in
-node_7_F2_out = node_6_F1_in
-node_7_F3_out = node_6_F1_in
-node_7_F0_out = node_6_F1_in
+node_7_F2_out = node_6_F2_in
+node_7_F3_out = node_6_F3_in
+node_7_F0_out = node_6_F0_in
 
 ## assumptions:
 node_7_assumptions = And(
     node_7_F1_in + node_7_F2_in + node_7_F3_in + node_7_F0_in <= 1,
-    node_7_F1_in + node_7_F2_in + node_7_F3_in + node_7_F0_in >= 0)
+    node_7_F1_in + node_7_F2_in + node_7_F3_in + node_7_F0_in >= 0,
+    node_7_F1_in >= 0, node_7_F1_in <= 1,
+    node_7_F2_in >= 0, node_7_F2_in <= 1,
+    node_7_F3_in >= 0, node_7_F3_in <= 1,
+    node_7_F0_in >= 0, node_7_F0_in <= 1)
 
 ## guarantees:
 node_7_guarantees = And(
     node_7_F0_in == node_7_F0_out,
-        node_7_F1_in == node_7_F1_out,
-            node_7_F2_in == node_7_F2_out,
-                node_7_F3_in == node_7_F3_out)
+    node_7_F1_in == node_7_F1_out,
+    node_7_F2_in == node_7_F2_out,
+    node_7_F3_in == node_7_F3_out,
+    node_7_F1_out >= 0, node_7_F1_out <= 1,
+    node_7_F2_out >= 0, node_7_F2_out <= 1,
+    node_7_F3_out >= 0, node_7_F3_out <= 1,
+    node_7_F0_out >= 0, node_7_F0_out <= 1,
+    node_7_F1_out + node_7_F2_out + node_7_F3_out + node_7_F0_out <= 1,
+    node_7_F1_out + node_7_F2_out + node_7_F3_out + node_7_F0_out >= 0,
+    node_7_F1_in + node_7_F2_in + node_7_F3_in + node_7_F0_in == node_7_F1_out + node_7_F2_out + node_7_F3_out + node_7_F0_out)
 
 ################################################################################
 ################################################################################
@@ -175,15 +227,34 @@ node_2_assumptions = And(
     node_2_F1_in + node_2_F2_in + node_2_F3_in + node_2_F0_in +
     node_2_F1_inf + node_2_F2_inf + node_2_F3_inf + node_2_F0_inf <= 1,
     node_2_F1_in + node_2_F2_in + node_2_F3_in + node_2_F0_in +
-    node_2_F1_inf + node_2_F2_inf + node_2_F3_inf + node_2_F0_inf >= 0)
+    node_2_F1_inf + node_2_F2_inf + node_2_F3_inf + node_2_F0_inf >= 0,
+    node_2_F0_in >= 0, node_2_F0_in <= 1,
+    node_2_F1_in >= 0, node_2_F1_in <= 1,
+    node_2_F2_in >= 0, node_2_F2_in <= 1,
+    node_2_F3_in >= 0, node_2_F3_in <= 1,
+    node_2_F0_inf >= 0, node_2_F0_inf <= 1,
+    node_2_F1_inf >= 0, node_2_F1_inf <= 1,
+    node_2_F2_inf >= 0, node_2_F2_inf <= 1,
+    node_2_F3_inf >= 0, node_2_F3_inf <= 1)
 
 ## guarantees:
 node_2_guarantees = And(
     node_2_F0_in + node_2_F0_inf == node_2_F0_out + node_2_F0_outf,
     node_2_F1_in + node_2_F1_inf == node_2_F1_out + node_2_F1_outf,
     node_2_F2_in + node_2_F2_inf == node_2_F2_out + node_2_F2_outf,
-    node_2_F3_in + node_2_F3_inf == node_2_F3_out + node_2_F3_outf)
-
+    node_2_F3_in + node_2_F3_inf == node_2_F3_out + node_2_F3_outf,
+    node_2_F0_out >= 0, node_2_F0_out <= 1,
+    node_2_F1_out >= 0, node_2_F1_out <= 1,
+    node_2_F2_out >= 0, node_2_F2_out <= 1,
+    node_2_F3_out >= 0, node_2_F3_out <= 1,
+    node_2_F0_outf >= 0, node_2_F0_outf <= 1,
+    node_2_F1_outf >= 0, node_2_F1_outf <= 1,
+    node_2_F2_outf >= 0, node_2_F2_outf <= 1,
+    node_2_F3_outf >= 0, node_2_F3_outf <= 1,
+    node_2_F1_in + node_2_F2_in + node_2_F3_in + node_2_F0_in +
+    node_2_F1_inf + node_2_F2_inf + node_2_F3_inf + node_2_F0_inf == 
+    node_2_F1_out + node_2_F2_out + node_2_F3_out + node_2_F0_out +
+    node_2_F1_outf + node_2_F2_outf + node_2_F3_outf + node_2_F0_outf)
 
 ################################################################################
 ################################################################################
@@ -215,14 +286,34 @@ node_3_assumptions = And(
     node_3_F1_in + node_3_F2_in + node_3_F3_in + node_3_F0_in +
     node_3_F1_inf + node_3_F2_inf + node_3_F3_inf + node_3_F0_inf <= 1,
     node_3_F1_in + node_3_F2_in + node_3_F3_in + node_3_F0_in +
-    node_3_F1_inf + node_3_F2_inf + node_3_F3_inf + node_3_F0_inf >= 0)
+    node_3_F1_inf + node_3_F2_inf + node_3_F3_inf + node_3_F0_inf >= 0,
+    node_3_F0_in >= 0, node_3_F0_in <= 1,
+    node_3_F1_in >= 0, node_3_F1_in <= 1,
+    node_3_F2_in >= 0, node_3_F2_in <= 1,
+    node_3_F3_in >= 0, node_3_F3_in <= 1,
+    node_3_F0_inf >= 0, node_3_F0_inf <= 1,
+    node_3_F1_inf >= 0, node_3_F1_inf <= 1,
+    node_3_F2_inf >= 0, node_3_F2_inf <= 1,
+    node_3_F3_inf >= 0, node_3_F3_inf <= 1)
 
 ## guarantees:
 node_3_guarantees = And(
     node_3_F0_in + node_3_F0_inf == node_3_F0_out + node_3_F0_outf,
     node_3_F1_in + node_3_F1_inf == node_3_F1_out + node_3_F1_outf,
     node_3_F2_in + node_3_F2_inf == node_3_F2_out + node_3_F2_outf,
-    node_3_F3_in + node_3_F3_inf == node_3_F3_out + node_3_F3_outf)
+    node_3_F3_in + node_3_F3_inf == node_3_F3_out + node_3_F3_outf,
+    node_3_F0_out >= 0, node_3_F0_out <= 1,
+    node_3_F1_out >= 0, node_3_F1_out <= 1,
+    node_3_F2_out >= 0, node_3_F2_out <= 1,
+    node_3_F3_out >= 0, node_3_F3_out <= 1,
+    node_3_F0_outf >= 0, node_3_F0_outf <= 1,
+    node_3_F1_outf >= 0, node_3_F1_outf <= 1,
+    node_3_F2_outf >= 0, node_3_F2_outf <= 1,
+    node_3_F3_outf >= 0, node_3_F3_outf <= 1,
+    node_3_F1_in + node_3_F2_in + node_3_F3_in + node_3_F0_in +
+    node_3_F1_inf + node_3_F2_inf + node_3_F3_inf + node_3_F0_inf == 
+    node_3_F1_out + node_3_F2_out + node_3_F3_out + node_3_F0_out +
+    node_3_F1_outf + node_3_F2_outf + node_3_F3_outf + node_3_F0_outf)
 
 ################################################################################
 ################################################################################
@@ -247,7 +338,11 @@ node_8_F0_out2 = node_7_F0_in
 ## assumptions:
 node_8_assumptions = And(
     node_8_F1_in + node_8_F2_in + node_8_F3_in + node_8_F0_in <= 1,
-    node_8_F1_in + node_8_F2_in + node_8_F3_in + node_8_F0_in >= 0
+    node_8_F1_in + node_8_F2_in + node_8_F3_in + node_8_F0_in >= 0,
+    node_8_F1_in >= 0, node_8_F1_in <= 1,
+    node_8_F2_in >= 0, node_8_F2_in <= 1,
+    node_8_F3_in >= 0, node_8_F3_in <= 1,
+    node_8_F0_in >= 0, node_8_F0_in <= 1
 )
 
 ## guarantees:
@@ -288,11 +383,22 @@ node_9_assumptions = And(
     node_9_F1_in1 + node_9_F2_in1 + node_9_F3_in1 + node_9_F0_in1 +
     node_9_F1_in2 + node_9_F2_in2 + node_9_F3_in2 + node_9_F0_in2 <= 1,
     node_9_F1_in1 + node_9_F2_in1 + node_9_F3_in1 + node_9_F0_in1 +
-    node_9_F1_in2 + node_9_F2_in2 + node_9_F3_in2 + node_9_F0_in2 >= 0
+    node_9_F1_in2 + node_9_F2_in2 + node_9_F3_in2 + node_9_F0_in2 >= 0,
+    node_9_F1_in1 >= 0, node_9_F1_in1 <= 1,
+    node_9_F2_in1 >= 0, node_9_F2_in1 <= 1,
+    node_9_F3_in1 >= 0, node_9_F3_in1 <= 1,
+    node_9_F0_in1 >= 0, node_9_F0_in1 <= 1,
+    node_9_F1_in2 >= 0, node_9_F1_in2 <= 1,
+    node_9_F2_in2 >= 0, node_9_F2_in2 <= 1,
+    node_9_F3_in2 >= 0, node_9_F3_in2 <= 1,
+    node_9_F0_in2 >= 0, node_9_F0_in2 <= 1
 )
 
 ## guarantees:
 node_9_guarantees = And(
+        node_9_F1_out + node_9_F2_out + node_9_F3_out + node_9_F0_out ==
+    node_9_F1_in1 + node_9_F2_in1 + node_9_F3_in1 + node_9_F0_in1 +
+    node_9_F1_in2 + node_9_F2_in2 + node_9_F3_in2 + node_9_F0_in2,
     node_9_F1_out == node_9_F1_in1 + node_9_F1_in2,
     node_9_F2_out == node_9_F2_in1 + node_9_F2_in2,
     node_9_F3_out == node_9_F3_in1 + node_9_F3_in2,
@@ -344,17 +450,20 @@ node_1_guarantees = And(
     node_1_F1_out1 == 0,
     node_1_F2_out1 == 0,
     node_1_F3_out1 == 0,
-    node_1_F0_out1 == 0,
+    node_1_F0_out1 >= 0,
+    node_1_F0_out1 <= 1,
+    node_1_F1_out2 == 0,
+    node_1_F2_out2 == 0,
+    node_1_F3_out2 == 0,
+    node_1_F0_out2 >= 0,
+    node_1_F0_out2 <= 1,
     node_1_F1_sinked == node_1_F1_in1 + node_1_F1_in2,
     node_1_F2_sinked == node_1_F2_in1 + node_1_F2_in2,
-    node_1_F3_sinked == node_1_F3_in1 + node_1_F3_in2,
-    node_1_F3_sinked >= 0.3,
-    node_1_F2_sinked >= 0.3,
-    node_1_F1_sinked >= 0.3
+    node_1_F3_sinked == node_1_F3_in1 + node_1_F3_in2
 )
 
 ## Contracts saturation.
-node_0_guarantees = Implies(node_0_assumptions, node_0_guarantees)
+node_0_guarantees = And(node_0_assumptions, node_0_guarantees)
 node_1_guarantees = Implies(node_1_assumptions, node_1_guarantees)
 node_2_guarantees = Implies(node_2_assumptions, node_2_guarantees)
 node_3_guarantees = Implies(node_3_assumptions, node_3_guarantees)
@@ -370,9 +479,9 @@ node_9_guarantees = Implies(node_9_assumptions, node_9_guarantees)
 warehouse_assumptions = True;
 
 warehouse_guarantees = And(
-    node_1_F3_sinked >= 0.3,
-    node_1_F2_sinked >= 0.3,
-    node_1_F1_sinked >= 0.3
+    node_1_F3_sinked >= 0.1,
+    node_1_F2_sinked >= 0.1,
+    node_1_F1_sinked >= 0.1
 )
 
 warehouse_guarantees = Implies(warehouse_assumptions, warehouse_guarantees)
@@ -388,7 +497,8 @@ system_guarantees = And(
     node_6_guarantees,
     node_7_guarantees,
     node_8_guarantees,
-    node_9_guarantees
+    node_9_guarantees,
+    warehouse_guarantees
 )
 
 system_assumptions = And(
@@ -401,7 +511,8 @@ system_assumptions = And(
     node_6_assumptions,
     node_7_assumptions,
     node_8_assumptions,
-    node_9_assumptions
+    node_9_assumptions,
+    warehouse_assumptions
 )
 
 system_assumptions = Or(
@@ -411,5 +522,11 @@ system_assumptions = Or(
 
 system_guarantees = Implies(system_assumptions, system_guarantees)
 
-solve(And(system_assumptions, system_guarantees))
+#print(system_guarantees)
+#print("Assumptions: ")
+#solve(system_assumptions)
+#print("Guarantees")
+#solve(system_guarantees)
 
+print("A and G")
+solve(And(system_assumptions, system_guarantees))
